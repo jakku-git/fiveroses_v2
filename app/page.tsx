237b005dd94499e886b3a7f3b702a3cf54dcbde6
jakku-git <jvkku@hotmail.com>
2025-03-16 04:39:11 +1100

    hero text

--- app/page.tsx
+++ app/page.tsx
@@ -54,8 +54,6 @@ export default function Home() {
         <div className="container mx-auto px-4">
           <h2 className="text-4xl md:text-5xl font-bold mb-12">Our Team</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            //<ThreeDCard />
-            //<CardSpotlight />
             <EvervaultCard />
           </div>
         </div>
