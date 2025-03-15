a94219a1b2995b020a6cdf69b9b35c69ce06c7d1
jakku-git <jvkku@hotmail.com>
2025-03-16 01:13:38 +1100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { BackgroundBoxes } from "@/components/ui-components/background-boxes";
 import { ThreeDCard } from "@/components/ui-components/3d-card";
 import { AppleCardsCarousel } from "@/components/ui-components/apple-cards-carousel";
+import { BackgroundGradientAnimation } from "@/components/ui-components/background-gradient-animation";
 import { BentoGrid } from "@/components/ui-components/bento-grid";
 import { CanvasReveal } from "@/components/ui-components/canvas-reveal";
 import { CardHoverEffect } from "@/components/ui-components/card-hover-effect";
